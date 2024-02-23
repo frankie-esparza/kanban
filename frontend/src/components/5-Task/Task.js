cb969df151ffc83b54c6e2fa6384b4aca897faa2
nudibranchy <frankietown@gmail.com>
2024-02-23 12:11:47 -0800

    Item component used to create Task and Subtask

--- frontend/src/components/5-Task/Task.js
+++ frontend/src/components/5-Task/Task.js
@@ -1,71 +1,9 @@
-import React, { useContext, useEffect, useState, memo } from 'react';
-import useFormState from '../../hooks/useFormState.js';
-import { KanbanContext } from '../../contexts/KanbanContext.js';
-import { getEditablePropsFromItemType, getInitialFormState } from '../../helpers/formHelpers.js';
-import axios from 'axios';
-import ItemCard from '../0-subcomponents/ItemCard.js';
-import EditDialog from '../0-subcomponents/EditDialog.js';
+import React, { memo } from 'react';
+import Item from '../0-subcomponents/Item.js';
 
 function Task({ task }) {
-    const port = 5000;
-    const itemType = 'task';
-    const { statuses, boards, tasks, subtasks, editItem, deleteItem } = useContext(KanbanContext);
-    const [subtasksOfTask, setSubtasksOfTask] = useState([]);
-
-    // Get Subtasks & store in Local State
-    useEffect(() => {
-        const getSubtasksOfTask = async (taskId) => {
-            axios
-                .get(`http://localhost:${port}/subtasks?task_id=${taskId}`)
-                .then((res) => setSubtasksOfTask(res.data))
-                .catch((err) => console.error(err.message))
-        }
-        getSubtasksOfTask(task.id);
-    }, [task.id])
-
-
-    // Event Handlers
-    const handleOpen = () => setFormOpen(true);
-    const handleClose = () => setFormOpen(false);
-    const handleDeleteClick = () => deleteItem(itemType, task.id);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        editItem(itemType, task.id, formState);
-        setFormOpen(false);
-        handleFormReset();
-    }
-
-    // Form State
-    const kanban = { statuses, boards, tasks, subtasks };
-    const editableProps = getEditablePropsFromItemType(itemType); // some props like id can't be edited
-    let initialFormState = getInitialFormState(editableProps, task, itemType, kanban);
-    const [formState, handleInputChange, handleFormReset] = useFormState({ ...initialFormState });
-    const [formOpen, setFormOpen] = useState(false);
-
     return (
-        <>
-            <ItemCard
-                item={task}
-                numSubtasks={subtasksOfTask.length}
-                handleOpen={handleOpen}
-            />
-            {formOpen &&
-                <>
-                    <EditDialog
-                        formState={formState}
-                        formOpen={formOpen}
-                        handleClose={handleClose}
-                        handleDeleteClick={handleDeleteClick}
-                        handleInputChange={handleInputChange}
-                        handleSubmit={handleSubmit}
-                        subtasks={subtasksOfTask}
-                        item={task}
-                        itemType={itemType}
-                    />
-                </>
-            }
-        </>
+        <Item item={task} itemType={'task'} />
     )
 }
 
--- frontend/src/components/6-Subtask/Subtask.js
+++ frontend/src/components/6-Subtask/Subtask.js
@@ -1,57 +1,9 @@
-import React, { useContext, useEffect, useState, memo } from 'react';
-import useFormState from '../../hooks/useFormState.js';
-import { KanbanContext } from '../../contexts/KanbanContext.js';
-import { getEditablePropsFromItemType, getInitialFormState } from '../../helpers/formHelpers.js';
-import ItemCard from '../0-subcomponents/ItemCard.js';
-import EditDialog from '../0-subcomponents/EditDialog.js';
+import React, { memo } from 'react';
+import Item from '../0-subcomponents/Item.js';
 
 function Subtask({ subtask }) {
-    const itemType = 'subtask';
-    const id = subtask.id;
-    const { statuses, boards, tasks, subtasks, editItem, deleteItem } = useContext(KanbanContext);
-
-    // Event Handlers
-    const handleOpen = () => setFormOpen(true);
-    const handleClose = () => setFormOpen(false);
-    const handleDeleteClick = () => deleteItem(itemType, id);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        editItem(itemType, id, formState);
-        setFormOpen(false);
-        handleFormReset();
-    }
-
-    // Form State
-    const kanban = { statuses, boards, tasks, subtasks };
-    const editableProps = getEditablePropsFromItemType(itemType); // some props like id can't be edited
-    let initialFormState = getInitialFormState(editableProps, subtask, itemType, kanban);
-    const [formState, handleInputChange, handleFormReset] = useFormState({ ...initialFormState });
-    const [formOpen, setFormOpen] = useState(false);
-
     return (
-        <>
-            <ItemCard
-                item={subtask}
-                itemType={itemType}
-                handleOpen={handleOpen}
-            />
-            {formOpen &&
-                <>
-                    <EditDialog
-                        formState={formState}
-                        formOpen={formOpen}
-                        handleClose={handleClose}
-                        handleDeleteClick={handleDeleteClick}
-                        handleInputChange={handleInputChange}
-                        handleSubmit={handleSubmit}
-                        item={subtask}
-                        itemType={itemType}
-                    />
-                </>
-            }
-        </>
+        <Item item={subtask} itemType={'subtask'} />
     )
 }
-
 export default memo(Subtask);
